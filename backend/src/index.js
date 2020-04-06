2340d34246ae90328f1b3c9523a56c469a54e5b7
higorgustavo10 <higor-gustavo_lp@hotmail.com>
2020-04-06 16:53:43 -0300

    Front-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
-const app = e
+const app = express();
 
 app.use(cors());
 app.use(express.json());
